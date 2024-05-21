9c434236249bad203414f6f6a2fb422cdbb27c3f
wlxklyh <linyanhou@qq.com>
2024-05-22 03:30:02 +0800

    [Init]

---
+++ Source/UnrealFramework/UnrealFramework.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealFramework.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealFramework, "UnrealFramework" );
